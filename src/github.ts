b490e97a64d71fa93a0953241f50e4170ef5e4e0
Robert Pratte <robert.pratte@fedex.com>
2023-08-24 10:54:42 -0400

    Adding markdown.ts

--- src/github.ts
+++ src/github.ts
@@ -1,7 +1,30 @@
-import * as github  from '@actions/github'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as version from './version'
+import * as markdown from './markdown'
 
-export async function createReleaseDraft(versionTag: string, repoToken: string, changelog: string): Promise<string> {
-    const octokit = new github.GitHub(repoToken)
+export async function createReleaseDraft(
+  versionTag: string,
+  repoToken: string,
+  changeLog: string
+): Promise<string> {
+  const octokit = github.getOctokit(repoToken)
 
-    const repsponse = await octokit.repos.createRelease({owner:'',repo:'',tag_name:''})
+  const response = await octokit.rest.repos.createRelease({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    tag_name: versionTag, // eslint-disable-line @typescript-eslint/camelcase
+    name: version.removePrefix(versionTag),
+    body: markdown.toUnorderedList(changeLog),
+    prerelease: version.isPrerelease(versionTag),
+    draft: true
+  })
+
+  if (response.status != 201) {
+    throw new Error(`Failed to create the release: ${response.status}`)
+  }
+
+  core.info(`Created release draft ${response.data.name}`)
+
+  return response.data.html_url
 }
